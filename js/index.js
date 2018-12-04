22a105acea674a4ee846946ff455a4d1aa41c676
inottn <inottn@outlook.com>
2018-12-04 11:01:29 +0800

    add: 实现点击特定元素跳转到指定视图的功能

--- js/index.js
+++ js/index.js
@@ -6,3 +6,29 @@ const dataYearAndMonthEl = dateEl.querySelector('.hero .nav .date .data-year-and
 const date = new Date()
 dataDayEl.innerHTML = date.getDate()
 dataYearAndMonthEl.innerHTML = `${date.getMonth() + 1} | ${date.getFullYear()}`
+
+// 跳转到指定视图
+const surfEl = document.querySelector('.surf')
+const travelEl = document.querySelector('.travel')
+const sleepEl = document.querySelector('.sleep')
+const shopEl = document.querySelector('.shop')
+const menuItems = document.querySelectorAll('.hero .nav .menu .menu-item')
+const menuSurfEl = menuItems[0]
+const menuTravelEl = menuItems[1]
+const menuSleepEl = menuItems[2]
+const menuShopEl = menuItems[3]
+const heroArrowDownEl = document.querySelector('.hero .arrow-down')
+
+const scrollEls = [menuSurfEl, menuTravelEl, menuSleepEl, menuShopEl, heroArrowDownEl]
+const scrollViews = [surfEl, travelEl, sleepEl, shopEl, surfEl]
+
+const scrollIntoView = (el, view) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault()
+    view.scrollIntoView({behavior: 'smooth', block: 'start'})
+  })
+}
+
+for (let i = 0; i < scrollEls.length; i++) {
+  scrollIntoView(scrollEls[i], scrollViews[i])
+}
