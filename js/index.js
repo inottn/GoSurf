b8af3399a83741db07582d2872ed31a9b38ea9b3
inottn <inottn@outlook.com>
2018-12-11 15:02:34 +0800

    refactor: 重构js文件组织结构

--- js/index.js
+++ js/index.js
@@ -1,34 +1,39 @@
-const dateEl = document.querySelector('.hero .nav .date')
-const dataDayEl = dateEl.querySelector('.hero .nav .date .data-day')
-const dataYearAndMonthEl = dateEl.querySelector('.hero .nav .date .data-year-and-month')
+(function () {
+  'use strict';
 
-// 让dateEl元素显示当前日期
-const date = new Date()
-dataDayEl.innerHTML = date.getDate()
-dataYearAndMonthEl.innerHTML = `${date.getMonth() + 1} | ${date.getFullYear()}`
+  const dateEl = document.querySelector('.hero .nav .date');
+  const dataDayEl = dateEl.querySelector('.hero .nav .date .data-day');
+  const dataYearAndMonthEl = dateEl.querySelector('.hero .nav .date .data-year-and-month');
 
-// 跳转到指定视图
-const surfEl = document.querySelector('.surf')
-const travelEl = document.querySelector('.travel')
-const sleepEl = document.querySelector('.sleep')
-const shopEl = document.querySelector('.shop')
-const menuItems = document.querySelectorAll('.hero .nav .menu .menu-item')
-const menuSurfEl = menuItems[0]
-const menuTravelEl = menuItems[1]
-const menuSleepEl = menuItems[2]
-const menuShopEl = menuItems[3]
-const heroArrowDownEl = document.querySelector('.hero .arrow-down')
+  // 让dateEl元素显示当前日期
+  const date = new Date();
+  dataDayEl.innerHTML = date.getDate();
+  dataYearAndMonthEl.innerHTML = `${date.getMonth() + 1} | ${date.getFullYear()}`;
 
-const scrollEls = [menuSurfEl, menuTravelEl, menuSleepEl, menuShopEl, heroArrowDownEl]
-const scrollViews = [surfEl, travelEl, sleepEl, shopEl, surfEl]
+  // 跳转到指定视图
+  const surfEl = document.querySelector('.surf');
+  const travelEl = document.querySelector('.travel');
+  const sleepEl = document.querySelector('.sleep');
+  const shopEl = document.querySelector('.shop');
+  const menuItems = document.querySelectorAll('.hero .nav .menu .menu-item');
+  const menuSurfEl = menuItems[0];
+  const menuTravelEl = menuItems[1];
+  const menuSleepEl = menuItems[2];
+  const menuShopEl = menuItems[3];
+  const heroArrowDownEl = document.querySelector('.hero .arrow-down');
 
-const scrollIntoView = (el, view) => {
-  el.addEventListener('click', (e) => {
-    e.preventDefault()
-    view.scrollIntoView({behavior: 'smooth', block: 'start'})
-  })
-}
+  const scrollEls = [menuSurfEl, menuTravelEl, menuSleepEl, menuShopEl, heroArrowDownEl];
+  const scrollViews = [surfEl, travelEl, sleepEl, shopEl, surfEl];
 
-for (let i = 0; i < scrollEls.length; i++) {
-  scrollIntoView(scrollEls[i], scrollViews[i])
-}
+  const scrollIntoView = (el, view) => {
+    el.addEventListener('click', (e) => {
+      e.preventDefault();
+      view.scrollIntoView({behavior: 'smooth', block: 'start'});
+    });
+  };
+
+  for (let i = 0; i < scrollEls.length; i++) {
+    scrollIntoView(scrollEls[i], scrollViews[i]);
+  }
+
+}());
---
+++ js/main.js
@@ -0,0 +1,34 @@
+const dateEl = document.querySelector('.hero .nav .date')
+const dataDayEl = dateEl.querySelector('.hero .nav .date .data-day')
+const dataYearAndMonthEl = dateEl.querySelector('.hero .nav .date .data-year-and-month')
+
+// 让dateEl元素显示当前日期
+const date = new Date()
+dataDayEl.innerHTML = date.getDate()
+dataYearAndMonthEl.innerHTML = `${date.getMonth() + 1} | ${date.getFullYear()}`
+
+// 跳转到指定视图
+const surfEl = document.querySelector('.surf')
+const travelEl = document.querySelector('.travel')
+const sleepEl = document.querySelector('.sleep')
+const shopEl = document.querySelector('.shop')
+const menuItems = document.querySelectorAll('.hero .nav .menu .menu-item')
+const menuSurfEl = menuItems[0]
+const menuTravelEl = menuItems[1]
+const menuSleepEl = menuItems[2]
+const menuShopEl = menuItems[3]
+const heroArrowDownEl = document.querySelector('.hero .arrow-down')
+
+const scrollEls = [menuSurfEl, menuTravelEl, menuSleepEl, menuShopEl, heroArrowDownEl]
+const scrollViews = [surfEl, travelEl, sleepEl, shopEl, surfEl]
+
+const scrollIntoView = (el, view) => {
+  el.addEventListener('click', (e) => {
+    e.preventDefault()
+    view.scrollIntoView({behavior: 'smooth', block: 'start'})
+  })
+}
+
+for (let i = 0; i < scrollEls.length; i++) {
+  scrollIntoView(scrollEls[i], scrollViews[i])
+}
